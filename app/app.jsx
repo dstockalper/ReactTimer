28e6dd4543a51216ede60cd2c967a4172d6005ab
Doug Stockalper <dstockalper@gmail.com>
2017-05-09 12:32:17 -0700

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// Access components from the react-router library (which we installed via npm install react-router)
+// ES5 syntax
+// var Route = require('react-router').Route;
+// var Router = require('react-router').Router;
+
+// ES6 "Destructuring" syntax
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+
+// App css
+require('style!css!sass!applicationStyles');
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+ReactDOM.render(
+	<Router history={hashHistory}>
+		<Route path="/" component={Main}>
+		</Route>
+	</Router>,
+	document.getElementById('app')
+);
